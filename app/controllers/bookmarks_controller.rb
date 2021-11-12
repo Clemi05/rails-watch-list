ab94b78e877e136f552b35a28932fcde32420baa
Clement Azalbert <clementazalbert04@gmail.com>
2021-11-12 12:21:29 +0100

    Watch list app - updated Front (CSS, HTML)

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,12 +1,13 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: %i[new create]
+
   def new
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    @list = List.find(params[:list_id])
     @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list)
@@ -16,7 +17,6 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
     redirect_to list_path(@bookmark.list)
   end
@@ -25,5 +25,13 @@ class BookmarksController < ApplicationController
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -22,6 +22,10 @@ class ListsController < ApplicationController
 
   private
 
+  def set_list
+    @list = List.find(params[:id])
+  end
+
   def list_params
     params.require(:list).permit(:name)
   end
